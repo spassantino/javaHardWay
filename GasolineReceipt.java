dbdea054b47dc1c2b90af0477871f8bbffdc643a
Sarah G Passantino <sarahgpassantino@Sarahs-MacBook-Pro.local>
2016-09-11 19:01:30 -0600

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      King Soopers      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| 2016-09-08   10:15PM	 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:	   18.07 |" );
+		System.out.println( "| Price/gallon: $ 2.04   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Fuel total: $36.86     |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
