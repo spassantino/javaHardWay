96e0c2688a6bd3c9e57551a1b8c75163bfbef9eb
Sarah G Passantino <sarahgpassantino@Sarahs-MacBook-Pro.local>
2016-09-11 18:45:31 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Sarah Passantino, am determined to learn how to code." );
+		System.out.println( "Today's date is September 8th" );
+	}
+}
