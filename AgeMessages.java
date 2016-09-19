86cce7c56d85df6fbb795736d92be6d12ed1d8f8
Sarah Passantino <spassantino@yahoo.com>
2016-09-18 18:45:23 -0600

    AgeMessages.java

---
+++ AgeMessages.java
@@ -0,0 +1,47 @@
+    import java.util.Scanner;
+        
+    public class AgeMessages {
+        public static void main( String[] args ) {
+            Scanner keyboard = new Scanner(System.in);
+            int age;
+                
+            System.out.print( "How old are you?" );
+            age = keyboard.nextInt();
+                
+            System.out.println( "You are: " );
+            if ( age < 13 ) {
+                System.out.println( "\ttoo young to create a Facebook account" );
+            }
+            if ( age > 13 ) {
+                System.out.println( "\tOld enough to create a Facebook account" );
+            }
+            if ( age < 16 ) {
+                System.out.println( "\ttoo young to get a driver's license" );
+            }
+            if ( age > 16 ) {
+                System.out.println( "\tOld enough to get a driver's license" );
+            }
+            if ( age < 18 ) {
+                System.out.println( "\ttoo young to get a tattoo" );
+            }
+            if ( age > 18 ) {
+                System.out.println( "\tOld enough to get a tattoo" );
+            }
+            if ( age < 35 ) {
+                System.out.println( "\ttoo young to run for President of the U.S." );
+                System.out.println( "\t\t(How sad!)" );
+            }
+            if ( age > 35 ) {
+                System.out.println( "\tOld enough to run for President of the U.S." );
+            }
+            if ( age > 64 ) {
+                System.out.println( "\tYou are old enough to retire!" );
+            }
+            if ( age < 65 ) {
+                System.out.println( "\ttoo young to retire" );
+                
+            // When I type in an age greater than 35 nothing prints because there is no formula given for greater than 35.
+            }
+        }
+    }
+    
