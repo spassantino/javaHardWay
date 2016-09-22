2b81043802d855c085d44cbe50bd3cf49650d0b5
Sarah Passantino <spassantino@yahoo.com>
2016-09-21 21:16:45 -0600

    add exercise 28 loops for error checking

---
+++ SafeSquareRoot.java
@@ -0,0 +1,33 @@
+            import java.util.Scanner;
+
+            public class SafeSquareRoot {
+                public static void main( String[] args ) {
+                    Scanner keyboard = new Scanner(System.in);
+                    double x, y;
+                    String ready;
+                    
+                    ready = "YES!";
+                    
+                    System.out.println("\nAre you ready?!?");
+                    keyboard.next();
+                    
+                    while ( ! ready.equals("YES!") ) {
+                    ready = keyboard.next();
+                   }
+                    
+                    System.out.print("Give me a number, and I'll find it's square root. ");
+                    System.out.print("(No negatives, please.) ");
+                    x = keyboard.nextDouble();
+
+                    while ( x < 0 ) {
+                        System.out.println("I won't take the square root of a negative.");
+                        System.out.print("\nNew number: ");
+                        x = keyboard.nextDouble();
+                   }
+
+                   y = Math.sqrt(x);
+
+                   System.out.println("The square root of "+x+" is "+y);
+              }
+         } 
+         
